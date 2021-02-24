82936745a2e20bead104e22a34cb6346dc92d94e
NANGGI <mqsora@naver.com>
2021-02-24 23:02:15 +0900

    Add files via upload

---
+++ js/class3.js
@@ -0,0 +1,16 @@
+class Square {
+   constructor (_width) {
+      this.width = _width;
+      this.height = _width;
+   }
+
+   static equals (a, b) {
+      return a.width * a.height === b.width * b.height;
+   }
+}
+
+let square1 = new Square(8);
+let square2 = new Square(9);
+
+console.log(square1);
+console.log(Square.equals(square1, square2));
