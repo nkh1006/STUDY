a86a4294f4e7013725f57e8a98e91cd405889504
NANGGI <mqsora@naver.com>
2021-02-28 23:03:26 +0900

    Add files via upload

---
+++ csharp/generic01/generic01/Calculator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace generic01
+{
+    class Calculator<T> where T : struct
+    {
+        public T Add(T a, T b)
+        {
+            dynamic da = a;
+            dynamic db = b;
+            T result = da + db;
+            return result;
+        }
+
+        public T Substract(T a, T b)
+        {
+            dynamic da = a;
+            dynamic db = b;
+            T result = da - db;
+            return result;
+        }
+
+        public T Multiply(T a, T b)
+        {
+            dynamic da = a;
+            dynamic db = b;
+            T result = da * db;
+            return result;
+        }
+
+        public T Divide(T a, T b)
+        {
+            dynamic da = a;
+            dynamic db = b;
+            T result = da / db;
+            return result;
+        }
+    }
+
+}
---
+++ csharp/generic01/generic01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace generic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var intSample = new Calculator<int>();
+            var x = intSample.Add(1, 5);
+            
+        }
+    }
+}
