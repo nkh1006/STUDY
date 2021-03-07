2a703de07d3f75941d3fe19e3388f3ca6f187ef8
NANGGI <mqsora@naver.com>
2021-03-08 00:14:08 +0900

    Add files via upload

---
+++ csharp/io_1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace io_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"c:\somefile.jpg";
+
+            // Copy
+            File.Copy(@"c:\temp\myfile.jpg", @"d:\temp\myfile.jpg");
+
+            // Delete
+            File.Delete(path);
+
+            // Exists
+            if (File.Exists(path))
+            {
+
+            }
+            var content = File.ReadAllText(path);
+
+
+            var fileInfo = new FileInfo(path);
+
+            // Copy
+            fileInfo.CopyTo("...");
+
+            // Delete
+            fileInfo.Delete();
+
+            // Exists
+            if (fileInfo.Exists)
+            {
+                Console.WriteLine("sample1");
+            }
+            else
+            {
+                Console.WriteLine("sample2");
+            }
+             
+        }
+    }
+}
