67a8a28cc23b03c161c19d34da52289366217fad
NANGGI <mqsora@naver.com>
2021-03-11 23:08:30 +0900

    Add files via upload

---
+++ csharp/list/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace list
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>() { 1, 2, 3, 4 };
+            numbers.Add(1);
+            numbers.AddRange(new int[3] { 5, 6, 7 });
+
+        }
+    }
+}
