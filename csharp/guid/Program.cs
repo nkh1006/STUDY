2ae1cfc0abf28f715222fded34e3fb13340ba685
NANGGI <mqsora@naver.com>
2021-03-15 21:44:14 +0900

    Add files via upload

---
+++ csharp/guid/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace guid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (var i = 1; i < 15; i++)
+            {
+                var uid = Guid.NewGuid().ToString();
+                Console.WriteLine($"#{i}. {uid}");
+            }
+        }
+    }
+}
