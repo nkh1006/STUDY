19e6a78a4ce86855a2c863df9d906710b7aed2c5
NANGGI <mqsora@naver.com>
2021-03-19 23:16:24 +0900

    Add files via upload

---
+++ asp.net_core/NetCore/NetCore.Services/Interfaces/IUser.cs
@@ -0,0 +1,12 @@
+﻿using NetCore.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetCore.Services.Interfaces
+{
+    public interface IUser
+    {
+        bool MatchTheUserInfo(LoginInfo login);
+    }
+}
---
+++ asp.net_core/NetCore/NetCore.Services/Svcs/UserService.cs
@@ -0,0 +1,40 @@
+﻿using NetCore.Data.DataModels;
+using NetCore.Data.Models;
+using NetCore.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace NetCore.Services.Svcs
+{
+    public class UserService : IUser
+    {
+        #region private methods
+        private IEnumerable<User> GetUserInfos()
+        {
+            return new List<User>()
+            {
+                new User()
+                {
+                    UserId = "jadejs",
+                    UserName = "김정수",
+                    UserEmail = "jadejskim@gmail.com",
+                    Password = "123456"
+                }
+            };
+        }
+
+        private bool checkTheUserInfo(string userId, string password)
+        {
+            return GetUserInfos().Where(u => u.UserId.Equals(userId) && u.Password.Equals(password)).Any();
+        }
+        #endregion
+
+        bool IUser.MatchTheUserInfo(LoginInfo login)
+        {
+            return checkTheUserInfo(login.UserId, login.Password);
+        }
+    }
+}
